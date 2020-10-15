348f6a4ba681e68584ae56519986dd01ea8c3657
Felipe Mateus <contato@felipefm32.com.br>
2020-10-14 23:22:36 -0300

    Save boat

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ from flask_restful import Resource, Api
 from pymongo import MongoClient
 from json import dumps
 from dotenv import load_dotenv
+from flask_jwt_extended import JWTManager
 
 load_dotenv()
 
@@ -15,6 +16,9 @@ from controllers.Login import Login
 
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = os.getenv("APP_SECRET")
+app.config['JWT_SECRET_KEY'] = os.getenv("APP_SECRET")
+
 app.mongo = MongoClient(os.getenv("MONGO_URL"))
 api = Api(app, prefix="/api/v1")
 ##
@@ -22,6 +26,9 @@ api.add_resource(Boat, '/boat/new')
 api.add_resource(User, '/user/new')
 
 api.add_resource(Login,'/login')
+
+#jwt
+jwt = JWTManager(app)
 
 if __name__ == '__main__':
     app.run(debug=os.getenv("DEBUG"))
